abecd5cf488108f5df963173f01b45e9116211fd
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-01-12 23:55:52 -0300

    criado o relacionamento inverso de um pra um na outra model

--- app/Models/Preference.php
+++ app/Models/Preference.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Preference extends Model
 {
     use HasFactory;
+
+    // Relacionamento One To One (Um pra um) inverso
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

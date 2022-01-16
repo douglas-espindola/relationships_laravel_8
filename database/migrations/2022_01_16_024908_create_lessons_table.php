59a2fb1e73a7875e1c3259fd73b383ba4efe4011
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-01-16 00:03:45 -0300

    criado campos na tabela de aulas e feito o relacionamento das keys entre a tabela de modulos

--- database/migrations/2022_01_16_024908_create_lessons_table.php
+++ database/migrations/2022_01_16_024908_create_lessons_table.php
@@ -15,6 +15,9 @@ class CreateLessonsTable extends Migration
     {
         Schema::create('lessons', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('module_id')->constrained('modules');
+            $table->string('name');
+            $table->string('video');
             $table->timestamps();
         });
     }

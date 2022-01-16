26388baa74e9fc9be32d3ee38a55aafea911503f
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-01-16 14:52:11 -0300

    criado campos do fillable e criado o relacionamento na model

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Module extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    // One to Many (Inverso) many to one (Muitos pra um)
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    // One to Many (Um pra muitos)
+    public function lessons()
+    {
+        return $this->hasMany(Lesson::class);
+    }
 }

bfd5850864a7ffd50abbeb9cf30f33667becf86d
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-01-16 00:03:05 -0300

    criado campos na tabela de modulos e feito o relacionamento das keys

--- database/migrations/2022_01_16_024849_create_modules_table.php
+++ database/migrations/2022_01_16_024849_create_modules_table.php
@@ -15,6 +15,8 @@ class CreateModulesTable extends Migration
     {
         Schema::create('modules', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('course_id')->constrained('courses');
             $table->timestamps();
         });
     }

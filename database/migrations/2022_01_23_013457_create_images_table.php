1eb231697f3d175c6e2871078bf695a886e2f019
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-01-22 22:43:19 -0300

    adicionado campo na model imagem

--- database/migrations/2022_01_23_013457_create_images_table.php
+++ database/migrations/2022_01_23_013457_create_images_table.php
@@ -15,7 +15,7 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('path');
             $table->morphs('imageable');
             $table->timestamps();
         });

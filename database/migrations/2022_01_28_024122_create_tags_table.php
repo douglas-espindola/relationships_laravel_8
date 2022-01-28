4135f42b7fad88aea84321dd6e02d351e3a9f98e
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-01-27 23:45:08 -0300

    criado campos na tabela de tags

--- database/migrations/2022_01_28_024122_create_tags_table.php
+++ database/migrations/2022_01_28_024122_create_tags_table.php
@@ -15,6 +15,8 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('color');
             $table->timestamps();
         });
     }
